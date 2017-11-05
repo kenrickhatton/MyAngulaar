57f39b673a2c42e1dc7d2d6c0dfcf9c6df57262a
Kenrick Hatton <Kenrick.hatton@gmail.com>
2017-11-05 15:30:55 +0000

    3rd update

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,25 @@
 angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
-        $scope.title = "Welcome To Angular Todo!"
+        $scope.title = "Welcome To Angular Todo!";
+    })
+    .controller('RegisterController', function($scope, $location, UserAPIService) {
+ 
+        $scope.registrationUser = {};
+        var URL = "https://morning-castle-91468.herokuapp.com/";
+ 
+        $scope.submitForm = function() {
+            if ($scope.registrationForm.$valid) {
+                $scope.registrationUser.username = $scope.user.username;
+                $scope.registrationUser.password = $scope.user.password;
+ 
+                UserAPIService.registerUser(URL + "accounts/register/", $scope.registrationUser).then(function(results) {
+                    $scope.data = results.data;
+                    alert("You have successfully registered to Angular Todo");
+                }).catch(function(err) {
+                	console.log(err);
+                    alert("Oops! Something went wrong!");
+                    
+                });
+            }
+        };
     });
