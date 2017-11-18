28fd71eba957a08652bde90bf1c27ccfaadd590a
Kenrick Hatton <Kenrick.hatton@gmail.com>
2017-11-18 16:04:02 +0000

    18.11.2017

--- js/controller.js
+++ js/controller.js
@@ -31,3 +31,9 @@ angular.module('RouteControllers', [])
             }
         }
     });
+
+/*And that’s it! Notice how in this case we could manually and safely remove our 
+test obj from local storage by right-clicking it in the dev-tools and choosing ‘delete’.
+If we wished to remove it using code, we could have used:
+
+-- store.remove("obj"); --*/
